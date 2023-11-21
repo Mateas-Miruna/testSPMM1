bdd6afa17fa0b09dd5fff1d23d310e000b78f163
Mateas-Miruna <106468793+Mateas-Miruna@users.noreply.github.com>
2023-11-21 08:37:07 +0200

    Initial commit

---
+++ src/main/java/com/example/testspmm1/TestSpmm1Application.java
@@ -0,0 +1,13 @@
+package com.example.testspmm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpmm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpmm1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testspmm1/TestSpmm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspmm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpmm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
